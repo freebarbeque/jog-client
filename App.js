015751578f51d15342e2e09acf4fb31fdd23396e
Richard Gill <richard@rgill.co.uk>
2017-03-23 15:20:33 +0000

    Initialised flow-typed & eslint.

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 // @flow
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -22,4 +22,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
