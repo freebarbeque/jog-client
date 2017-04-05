528466e898d81fe8591d7e3bdf299cc4aa63a38d
Richard Gill <richard@rgill.co.uk>
2017-04-05 09:07:00 +0100

    First draft of Firebase motor policy data structure.

--- src/types.js
+++ src/types.js
@@ -4,7 +4,9 @@ import type { Store as ReduxStore, Dispatch as ReduxDispatch } from 'redux'
 import type { NavigationAction } from 'react-navigation'
 import type { AuthAction } from './redux/auth/actionTypes'
 
-/* Redux */
+//
+// Redux
+//
 
 export type Action = AuthAction | NavigationAction
 
@@ -38,7 +40,9 @@ export type Store = ReduxStore<ReduxState, Action>;
 
 export type Dispatch = ReduxDispatch<Action>;
 
-/* Forms */
+//
+// Forms
+//
 
 export type FormField = {
   inputProps: $Subtype<Object>,
@@ -48,7 +52,12 @@ export type FormField = {
   validate?: (val: string) => string | null,
 }
 
-/* Firebase */
+export type ValuesMap = {[key: string] : string}
+export type ValidationErrorsMap = {[key: string]: string | null}
+
+//
+// Firebase
+//
 
 // https://firebase.google.com/docs/reference/js/firebase.User
 export type FirebaseUser = {
@@ -62,7 +71,50 @@ export type FirebaseUser = {
   uid: string,
 }
 
-/* react-navigation */
+//
+// Motor Policies
+//
+
+export type Conviction = {
+  date: number,
+  // Driving conviction codes: https://en.wikipedia.org/wiki/List_of_UK_driving_licence_endorsements
+  type: 'SP30' | 'AC10' | 'BA10'
+}
+
+// Describes a driver attached to a motor policy
+export type Driver = {
+  firstNames: string,
+  lastName: string,
+  dob: number,
+  main: boolean, // Is this the main driver on the policy?
+  convictions: Conviction[],
+  penaltyPoints: number,
+  licenseNumber: string,
+  licenseStartDate: string,
+  everSuspendend: boolean,
+}
+
+// /policies/${policyId}
+export type MotorPolicy = {
+  policyId: string, // Jogs identifier for the policy (guid?)
+  policyNo: string, // I would assume this is the insurer's own identifier? I know some will have non-numeric characters
+  vehicleRegistration: string,
+  expiryDate: number,
+  startDate: number,
+  jogCreatedDate: number, // Date added to jog as opposed to insurance start date
+  companyId: string,
+  levelOfCover: 'comprehensive' | 'tpft' | 'third-party',
+  excess: number,
+  drivers: Driver[],
+  noClaimsBonus: number, // Num. years.
+  documentPaths: string[], // Paths on firebase storage.
+  uid: string, // Firebase user id.
+  milesPerYear: number,
+}
+
+//
+// React Navigation
+//
 
 // navigation
 export type ReactNavigationProp = {
@@ -80,9 +132,3 @@ export type ReactNavProp = {
   routes: Route[]
 }
 
-//
-// Forms
-//
-
-export type ValuesMap = {[key: string] : string}
-export type ValidationErrorsMap = {[key: string]: string | null}
