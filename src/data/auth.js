4474b2aff6cb221b955248fa4bf3feb3e3030607
Richard Gill <richard@rgill.co.uk>
2017-04-07 09:33:02 +0100

    Verification email is now sent on sign up.

--- src/data/auth.js
+++ src/data/auth.js
@@ -43,7 +43,10 @@ export const signInAnonymously: () => Promise<void>
   = errorWrapper(() => firebase.auth().signInAnonymously())
 
 export const createUserWithEmailAndPassword: (string, string) => Promise<void>
-  = errorWrapper((email: string, password: string) => firebase.auth().createUserWithEmailAndPassword(email, password))
+  = errorWrapper(async (email: string, password: string) => {
+    const user = await firebase.auth().createUserWithEmailAndPassword(email, password)
+    await user.sendEmailVerification()
+  })
 
 export const signInWithEmailAndPassword: (string, string) => Promise<void>
   = errorWrapper((email, password) => firebase.auth().signInWithEmailAndPassword(email, password))
@@ -60,3 +63,4 @@ export const confirmPasswordReset : (code: string, password: string) => Promise<
 export function userSubscribe(callback: (user: FirebaseUser | null) => void): void {
   firebase.auth().onAuthStateChanged(callback)
 }
+
