d7b187dc9a7defb050468acb20c09bdbbe83f8dc
Richard Gill <richard@rgill.co.uk>
2017-04-07 09:06:15 +0100

    Added android launcher icons.

--- scripts/deploy-android-alpha.sh
+++ scripts/deploy-android-alpha.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd android &&
+cd android && \
+./gradlew clean && \
 # Generate unsigned APK
 ./gradlew assembleRelease && \
 # Clean up prior builds
@@ -8,6 +9,6 @@ rm -f app/build/outputs/apk/app-release-unsigned-aligned.apk && \
 # Generate aligned, unsigned APK
 zipalign -v -p 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk && \
 # Generate signed APK
-apksigner sign --ks ${JOG_KEY_STORE} --out app/app-release.apk app/build/outputs/apk/app-release-unsigned-aligned.apk && \
+apksigner sign --ks fastlane.jks --out app/app-release.apk app/build/outputs/apk/app-release-unsigned-aligned.apk && \
 # Upload meta data and APK
-fastlane supply --apk android/app/app-release.apk --track alpha --json_key ${JOG_GP_JSON_KEY} --package_name com.jog
+fastlane supply --apk app/app-release.apk --track alpha --json_key fastlane-privatekey.json --package_name com.jog
