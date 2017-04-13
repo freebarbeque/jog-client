b84fc401c33d72c29b32261626cce5b4a2131176
Richard Gill <richard@rgill.co.uk>
2017-04-13 10:31:51 +0100

    Hijack the android back button to move backwards in the navigation hierarchy.

--- App.js
+++ App.js
@@ -10,31 +10,18 @@ import initialiseFirebase from './src/data'
 import { BLUE } from './src/constants/palette'
 import RootNavigator from './src/navigators/RootNavigator'
 import { syncData } from './src/store/actions'
-import type { Route } from './src/types'
 
 initialiseFirebase()
 
 const store = createStore()
-
-function sumRoutes(routes: Route[]) {
-  return routes.length + _.sum(routes.map((r) => sumRoutes(r.routes || [])))
-}
 
 export default class JogApp extends Component {
   componentDidMount() {
     store.dispatch(syncData())
 
     BackAndroid.addEventListener('hardwareBackPress', () => {
-      const nav = store.getState().nav
-      const routes = nav.routes
-
-      const numRoutes = sumRoutes(routes)
-      console.log('numRoutes', numRoutes)
-      if (numRoutes > 1) {
-        store.dispatch(NavigationActions.back())
-        return true // Override default behaviour
-      }
-      return false // Default behavior e.g. it will exit the app as normal
+      store.dispatch(NavigationActions.back())
+      return true
     })
   }
 
