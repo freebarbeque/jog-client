e5d4909333ef5102693e5513b2b05c52c76f0de1
Richard Gill <richard@rgill.co.uk>
2017-04-14 13:07:00 +0100

    iOS: Upload policy document pdfs through document browser.
    This PR is missing the following:
    
    - Error handling.
    - Progress/loaders.
    
    Both will be made available in a seperate PR to keep the PRs smaller.

--- src/screens/PolicyDocumentsScreen/index.js
+++ src/screens/PolicyDocumentsScreen/index.js
@@ -1,35 +1,109 @@
 /* @flow */
 
 import React, { Component } from 'react'
-import { View, StyleSheet, TouchableHighlight, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Platform, NativeModules } from 'react-native'
 import { connect } from 'react-redux'
-import type { ReduxState } from 'jog/src/types'
-import Text from 'jog/src/components/Text'
-import { BLUE } from 'jog/src/constants/palette'
-import { MARGIN } from 'jog/src/constants/style'
+import DeviceInfo from 'react-native-device-info'
 import ImagePicker from 'react-native-image-picker'
-import Panel from '../../components/Panel'
-import { CREAM } from '../../constants/palette'
+import RNFetchBlob from 'react-native-fetch-blob'
+import firebase from 'firebase'
+
+import type { ReduxState, FirebaseUser, ReactNavigationProp } from 'jog/src/types'
+import Text from 'jog/src/components/Text'
+import { BLUE, CREAM } from 'jog/src/constants/palette'
+import { MARGIN } from 'jog/src/constants/style'
+import Panel from 'jog/src/components/Panel'
+
+const DocumentPicker = NativeModules.RNDocumentPicker
 
 type PolicyDocumentsScreenProps = {
   // dispatch: Dispatch,
-  // user: number,
+  user: FirebaseUser | null,
+  navigation: ReactNavigationProp
 };
 
 class PolicyDocumentsScreen extends Component {
   props: PolicyDocumentsScreenProps
+
+  handleBrowseFilesPress = () => {
+    const user = this.props.user
+    if (user) {
+      const policyId = this.props.navigation.state.params.policyId
+
+      if (policyId) {
+        if (Platform.OS === 'ios') {
+          DocumentPicker.show({
+            filetype: [
+              'com.adobe.pdf',
+            ],
+          }, (error, url) => {
+            if (!error) {
+              // RNFetchBlob doesn't like the file:/// protocol.
+              const split = url.split('file:///')
+              const filePath = split[1]
+              const fileName = filePath.split('/').pop()
+
+              RNFetchBlob.fs.readFile(filePath, 'base64').then((data) => {
+                const storagePath = `/policyDocuments/${user.uid}/${policyId}/${fileName}`
+                console.debug(`Storing file at "${storagePath}"`)
+                const ref = firebase.storage().ref(storagePath)
+                ref.putString(data, 'base64').then(() => {
+                  console.log('Upload success')
+                }).catch((err) => {
+                  console.error('Upload failure', err)
+                })
+              }).catch((err) => {
+                // TODO: Handle errors
+                console.error(err)
+              })
+            } else {
+              // TODO: Handle errors
+            }
+          })
+        } else {
+          throw new Error('NYI: Android file picker')
+        }
+      } else {
+        throw new Error('policyId is not present in the navigation params, therefore cannot upload the policy document')
+      }
+    } else {
+      throw new Error('Should not be able to upload without a user being logged in...')
+    }
+  }
+
+  handleUseCameraPress = () => { // TODO
+    const isSimulator = DeviceInfo.getModel() === 'Simulator'
+    // The simulator has no camera access
+    if (isSimulator) {
+      // eslint-disable-next-line no-unused-vars
+      ImagePicker.launchImageLibrary({}, (response) => {
+
+      })
+    } else {
+      // eslint-disable-next-line no-unused-vars
+      ImagePicker.launchCamera({}, (response) => {
+
+      })
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Panel style={styles.panel}>
           <Text style={styles.header}>Upload documents</Text>
-          <View style={styles.camera}>
+          <TouchableOpacity
+            style={styles.camera}
+            onPress={this.handleUseCameraPress}
+          >
             <Text style={styles.cameraText}>
               Use your camera
             </Text>
-          </View>
-          <TouchableOpacity style={styles.browseFilesButton}>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.browseFilesButton}
+            onPress={this.handleBrowseFilesPress}
+          >
             <View>
               <Text style={styles.browseFilesButtonText}>
                 Browse Files
@@ -54,7 +128,7 @@ const styles = StyleSheet.create({
     marginBottom: MARGIN.large
   },
   camera: {
-    height: 112,
+    height: 130,
     alignItems: 'center',
     justifyContent: 'center',
     borderStyle: 'dashed',
