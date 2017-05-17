5ec6b1dbe654294656767f812337a795e7533ea1
Richard Gill <richard@rgill.co.uk>
2017-05-17 14:29:18 +0100

    privacy policy + terms & conditions

--- src/screens/SettingsScreen/SettingsPrivacyPolicySection.js
+++ src/screens/SettingsScreen/SettingsPrivacyPolicySection.js
@@ -1,10 +1,9 @@
 /* @flow */
 
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, WebView } from 'react-native'
 import { connect } from 'react-redux'
 import type { ReduxState, Dispatch } from 'jog/src/types'
-import Text from 'jog/src/components/Text'
 import { BLUE, VERY_LIGHT_GRAY } from 'jog/src/constants/palette'
 import { MARGIN } from 'jog/src/constants/style'
 
@@ -12,7 +11,9 @@ type SettingsPrivacyPolicySectionProps = {
   dispatch: Dispatch,
 };
 
-type SettingsPrivacyPolicySectionState = {};
+type SettingsPrivacyPolicySectionState = {
+  webviewHeight: number
+};
 
 class SettingsPrivacyPolicySection extends Component {
   props: SettingsPrivacyPolicySectionProps
@@ -20,38 +21,29 @@ class SettingsPrivacyPolicySection extends Component {
 
   constructor(props: SettingsPrivacyPolicySectionProps) {
     super(props)
-    this.state = {}
+    this.state = {
+      webViewHeight: 100 // default height, can be anything
+    }
+  }
+
+  updateWebViewHeight = (event) => {
+  // jsEvaluationValue contains result of injected JS
+    this.setState({ webViewHeight: parseInt(event.jsEvaluationValue, 10) })
   }
 
   render() {
+    const height = this.state.webViewHeight
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          Nam rutrum dui at erat lacinia cursus. Aliquam at sagittis mauris. Mauris et dictum lorem.
-          Sed venenatis justo ex, ac vehicula ipsum elementum id. Sed sit amet rhoncus arcu.
-          Etiam tincidunt velit et nibh mattis bibendum nec in risus. Nulla ante ex, egestas non libero sit amet, egestas hendrerit libero.
-          Aliquam ligula sem, rutrum non felis sed, suscipit sodales tellus. Sed in interdum dolor, sit amet feugiat nunc.
-        </Text>
-        <Text style={styles.text}>
-          Ut sed elit bibendum, congue nibh convallis, mollis dui. Ut pellentesque finibus magna, sed gravida erat blandit sed.
-          Nullam quis sapien a lectus rhoncus posuere. Ut sed malesuada risus. Donec hendrerit urna ac vulputate tempor.
-          Nam condimentum enim tellus, eu vulputate sapien maximus in. Donec placerat neque mauris.
-          Vestibulum maximus augue at nibh pellentesque, vel mollis nibh iaculis. Suspendisse id vulputate erat.
-          Aenean rhoncus euismod elit a gravida. Pellentesque sit amet feugiat eros, et aliquet tellus. Nulla facilisi.
-          Sed faucibus arcu a massa gravida, sit amet convallis magna fermentum. Donec porta dignissim ligula et scelerisque.
-          Phasellus euismod sit amet dui non finibus. Mauris nec neque leo.
-        </Text>
-        <Text style={styles.text}>
-          Mauris rhoncus turpis leo, laoreet tristique nulla tristique in. In sagittis lectus bibendum, gravida nulla sed, tincidunt sem.
-          In ligula mi, congue ac tortor vel, gravida placerat mi. Etiam in varius urna.
-          Pellentesque elit nisi, tristique at maximus in, pellentesque vitae sem.
-          Nam feugiat posuere sem, a aliquam sem facilisis id. Nulla et nunc est.
-          Sed dapibus, est eget convallis suscipit, nisl sem bibendum turpis, at semper ipsum orci vel mi.
-          Suspendisse eget velit faucibus, ullamcorper dolor in, vehicula nibh. Aenean quis sapien porta mi congue blandit.
-          Aenean feugiat aliquet sagittis. Vestibulum quis tincidunt mi, et posuere ligula.
-          In velit justo, gravida at erat sit amet, convallis commodo leo. Proin pharetra lorem libero.
-          Nam vel nulla feugiat, suscipit dui id, rutrum massa. Etiam sit amet imperdiet nulla, venenatis sodales libero.
-        </Text>
+        <WebView
+          style={{ width: '100%', height, backgroundColor: VERY_LIGHT_GRAY }}
+          source={require('./privacypolicy.html')}
+          injectedJavaScript="document.body.scrollHeight;"
+          scrollEnabled={false}
+          onNavigationStateChange={this.updateWebViewHeight}
+          automaticallyAdjustContentInsets
+        />
       </View>
     )
   }
@@ -61,7 +53,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: VERY_LIGHT_GRAY,
-    padding: MARGIN.large
+    padding: MARGIN.base,
+    flexDirection: 'row'
   },
   text: {
     color: BLUE,
