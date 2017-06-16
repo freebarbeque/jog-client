a3821b91c381abc5569a37cba2edd6beb9b4444a
Richard Gill <richard@rgill.co.uk>
2017-06-16 16:26:22 +0100

    Configured settings screen.

--- src/web/screens/MainScreen/SettingsTab.js
+++ src/web/screens/MainScreen/SettingsTab.js
@@ -1,23 +1,74 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
-const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-}
+import Collapsible from '../../components/Collapsible'
+import { MARGIN } from '../../../common/constants/style'
+import { WHITE } from '../../../common/constants/palette'
+
+// language=SCSS prefix=dummy{ suffix=}
+const Container = styled.div`
+  padding: ${MARGIN.base}px;
+  background-color: ${WHITE};
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+`
 
 export default class SettingsTab extends Component {
   render() {
     return (
-      <div style={{ flex: 1 }}>
-        <h2 style={styles.headline}>Tab Two</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
+      <Container className="settings-tab-container">
+        <Collapsible trigger="My Profile">
+          <p>
+            This is the collapsible content. It can be any element or React
+            component you like.
+          </p>
+          <p>
+            It can even be another Collapsible component. Check out the next
+            section!
+          </p>
+        </Collapsible>
+        <Collapsible trigger="About Us">
+          <p>
+            This is the collapsible content. It can be any element or React
+            component you like.
+          </p>
+          <p>
+            It can even be another Collapsible component. Check out the next
+            section!
+          </p>
+        </Collapsible>
+        <Collapsible trigger="Terms and conditions">
+          <p>
+            This is the collapsible content. It can be any element or React
+            component you like.
+          </p>
+          <p>
+            It can even be another Collapsible component. Check out the next
+            section!
+          </p>
+        </Collapsible>
+        <Collapsible trigger="Cookies policy">
+          <p>
+            This is the collapsible content. It can be any element or React
+            component you like.
+          </p>
+          <p>
+            It can even be another Collapsible component. Check out the next
+            section!
+          </p>
+        </Collapsible>
+        <Collapsible trigger="Privacy policy">
+          <p>
+            This is the collapsible content. It can be any element or React
+            component you like.
+          </p>
+          <p>
+            It can even be another Collapsible component. Check out the next
+            section!
+          </p>
+        </Collapsible>
+      </Container>
     )
   }
 }
