8a666818c3d10c4f659d19de7b0b5d477a429c35
pavelpashkovsky <pavelpashkovsky@gmail.com>
2017-10-24 17:06:57 +0300

    add: initial documents container

---
+++ src/screens/Documents/DocumentsContainer.ts
@@ -0,0 +1,23 @@
+import { connect } from "react-redux";
+import Documents from "./components/Documents";
+import { compose } from 'recompose';
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+};
+
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps)
+)(Documents);
+
+
+
---
+++ src/screens/Documents/components/Documents.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View, ScrollView } from 'react-native';
+
+interface Props {
+    navigation: any
+}
+
+const Documents: React.StatelessComponent<Props> = ({navigation}) => {
+    return (
+        <Text>Documents</Text>
+    )
+}
+
+export default Documents;
