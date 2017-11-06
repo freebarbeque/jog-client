8b765709d2009da378f86691f275d06a82bb95a9
Tom Mullen <tom@dvelp.co.uk>
2017-11-06 17:55:34 +0000

    Add TestFairy lib for builds
    Why:
    
    * To ship builds to TestFairy
    
    This change addresses the need by:
    
    * Adding the react-native-testfairy module
    * Updating xcode project inline with TestFairy recommendations

--- index.ios.js
+++ index.ios.js
@@ -3,4 +3,7 @@
 import { AppRegistry } from 'react-native'
 import App from './build'
 
+const TestFairy = require('react-native-testfairy');
+
+TestFairy.begin('de972f078461634a81a28f0a888ff58ad4e48b76')
 AppRegistry.registerComponent('ReactNativeTS', () => App)
