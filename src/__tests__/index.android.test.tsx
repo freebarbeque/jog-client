a0835ca16b64098e288d17d78d39da0685b983c0
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-10 10:05:55 +0300

    Fix lint errors in tests

--- src/__tests__/index.android.test.tsx
+++ src/__tests__/index.android.test.tsx
@@ -1,11 +1,11 @@
-import "react-native";
-import React from "react";
+import 'react-native';
+import React from 'react';
 // Note: test renderer must be required after react-native.
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
 
-import App from "../index.android";
+import App from '../index.android';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer.create(
     <App />
   );
--- src/__tests__/index.ios.test.tsx
+++ src/__tests__/index.ios.test.tsx
@@ -1,11 +1,11 @@
-import "react-native";
-import React from "react";
+import 'react-native';
+import React from 'react';
 // Note: test renderer must be required after react-native.
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
 
-import App from "../index.ios";
+import App from '../index.ios';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer.create(
     <App />
   );
