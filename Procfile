a8db23334c0860d16b65485025e2fe5458a5e8cc
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-15 20:48:23 +0300

    Define server to serve the app on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./node_modules/webpack-dev-server/bin/webpack-dev-server.js
+web: ./node_modules/webpack/bin/webpack.js && node server
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const indexPath = path.join(__dirname, '/../dist/index.html')
+const staticPath = path.join(__dirname, '/../dist')
+
+app.use(express.static(staticPath));
+app.get('/', function (_, res) {
+    res.sendFile(indexPath)
+})
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Started at ${process.env.PORT || 8080}`)
