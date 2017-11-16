3bbd63bbcc974ec276be3575beb4bd230f7128ea
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-16 11:06:15 +0300

    Make server to serve index.html on any route

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const indexPath = path.join(__dirname, '/../dist/index.html')
 const staticPath = path.join(__dirname, '/../dist')
 
 app.use(express.static(staticPath));
-app.get('/', function (_, res) {
+app.get('*', function (_, res) {
     res.sendFile(indexPath)
 })
 
