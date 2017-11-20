b2025a7cfae9501aa25ca7f8b96c088c7c90a958
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-20 14:02:02 +0300

    Add content and footer sections

--- src/web/screens/Policy/CreatePolicyScreen.tsx
+++ src/web/screens/Policy/CreatePolicyScreen.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import Header from 'src/web/components/Header';
 import Description from './components/Description';
+import styled from 'styled-components';
+import {CREAM} from '~/common/constants/palette';
+import Footer from 'src/web/components/Footer';
 
 interface ICreatePolicyScreenProps {
     className: string;
 }
 
+const Content = styled.div`
+  background-color: ${CREAM};
+`;
+
 export default (props: ICreatePolicyScreenProps) => (
     <div className={props.className}>
         <Header />
         <Description />
+        <Content>111</Content>
+        <Footer />
     </div>
 );
