6b4674fe50bd5dcfca7db72c554c3380a59a2c4a
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-24 16:59:17 +0300

    Define api saga uploadDocuments

--- src/common/api/documents.ts
+++ src/common/api/documents.ts
@@ -1,8 +1,26 @@
-import {get} from '../api/request';
-import {put} from 'redux-saga/effects';
+import {get, post} from '../api/request';
+import {put, select} from 'redux-saga/effects';
 import {setDocuments} from '../actions/documents';
+import {getSessionToken} from '../selectors/auth';
+import {IPendingDocument} from '../interfaces/documents';
 
 export function* fetchDocuments(userId: number, policyId: string) {
     const {body} = yield get(`users/${userId}/motor_policies/${policyId}/documents`);
     yield put(setDocuments(body.documents));
+}
+
+export function* uploadDocuments(userId: number, policyId: string, docs: IPendingDocument[]) {
+    const sessionToken = yield select(getSessionToken);
+
+    const headers = new Headers({
+        'Authorization': sessionToken,
+        'Accept': 'multipart/form-data',
+    });
+
+    yield docs.map(d => {
+        const body = new FormData();
+        body.append('data[type]', 'documents');
+        body.append('data[attributes][attachment]', d.file);
+        return post(`users/${userId}/motor_policies/${policyId}/documents`, body, headers, false);
+    })
 }
--- src/common/sagas/documents.ts
+++ src/common/sagas/documents.ts
@@ -1,15 +1,13 @@
 import {fork, put, race, select, take} from 'redux-saga/effects';
 import {UPLOAD_PENDING_DOCUMENTS} from '../constants/documents';
 import {getPendingDocuments} from '../selectors/documents';
-import {post} from '../api/request';
-import {getSessionToken, getUser} from '../selectors/auth';
+import {getUser} from '../selectors/auth';
 import {clearPendingDocuments, setDocumentSubmissionError, setIsLoading} from '../actions/documents';
-import {fetchDocuments} from '../api/documents';
+import {fetchDocuments, uploadDocuments} from '../api/documents';
 import {LOCATION_CHANGE} from 'react-router-redux';
 
 export function* documentsFlow(policyId: string) {
     const user = yield select(getUser);
-    const sessionToken = yield select(getSessionToken);
     yield fork(fetchDocuments, user.id, policyId);
 
     while (true) {
@@ -25,19 +23,10 @@ export function* documentsFlow(policyId: string) {
 
             yield put(setIsLoading(true));
             yield put(setDocumentSubmissionError(null));
+
             const docs = yield select(getPendingDocuments);
+            yield uploadDocuments(user.id, policyId, docs);
 
-            const headers = new Headers({
-                'Authorization': sessionToken,
-                'Accept': 'multipart/form-data',
-            });
-
-            yield docs.map(d => {
-                const body = new FormData();
-                body.append('data[type]', 'documents');
-                body.append('data[attributes][attachment]', d.file);
-                return post(`users/${user.id}/motor_policies/${policyId}/documents`, body, headers, false);
-            })
             yield fetchDocuments(user.id, policyId);
             yield put(clearPendingDocuments());
 
