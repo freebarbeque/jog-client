44aeef50f3f202c7957959b5659159dc91329335
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-29 19:09:53 +0300

    Add post code field

--- src/web/screens/UserDetails/components/AddressForm.tsx
+++ src/web/screens/UserDetails/components/AddressForm.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {reduxForm, Field} from 'redux-form';
 import FieldTitle from './FieldTitle';
 import styledForm from './styledForm';
-import Input from 'src/web/components/Forms/Input';
+import StyledInput from './StyledInput';
 
 interface IAddressFormProps {
     className: string;
@@ -12,6 +12,10 @@ const AddressForm = (props: IAddressFormProps) => {
     return (
         <form className={props.className}>
             <FieldTitle>Post Code</FieldTitle>
+            <Field
+                name="postcode"
+                component={StyledInput}
+            />
         </form>
     )
 };
