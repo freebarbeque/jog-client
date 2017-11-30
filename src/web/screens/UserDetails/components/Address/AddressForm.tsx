6a1b7fb0a7e5780c2b97dac0ed8532e514b4b644
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-11-30 13:03:59 +0300

    Define handleAddressFormSubmit in AddressSteps class

--- src/web/screens/UserDetails/components/Address/AddressForm.tsx
+++ src/web/screens/UserDetails/components/Address/AddressForm.tsx
@@ -6,20 +6,32 @@ import StyledInput from '../StyledInput';
 import {isPostCode} from '~/common/utils/userDetails';
 import {IAddressFormValues} from '~/common/interfaces/userDetails';
 const validate = require('validate.js');
+import RoundedButton from 'src/web/components/RoundedButton';
+import {MARGIN} from 'src/common/constants/style';
 
 interface IAddressFormProps {
     className: string;
+    handleSubmit: any;
 }
 
 const AddressForm = (props: IAddressFormProps) => {
     return (
-        <form className={props.className}>
+        <form className={props.className} onSubmit={props.handleSubmit}>
             <FieldTitle>Post Code</FieldTitle>
             <Field
                 name="postcode"
                 component={StyledInput}
             />
-
+            <RoundedButton
+                type="submit"
+                label="Next"
+                style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    marginTop: MARGIN.extraLarge,
+                }}
+                disabled={false}
+            />
         </form>
     )
 };
--- src/web/screens/UserDetails/components/Address/AddressSteps.tsx
+++ src/web/screens/UserDetails/components/Address/AddressSteps.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import AddressForm from './AddressForm';
+import {IAddressFormValues} from '~/common/interfaces/userDetails';
 
 interface IAddressStepsProps {
     currentStep: number
 }
 
-export default (props: IAddressStepsProps) => {
-    switch (props.currentStep) {
-        case 1:
-            return <AddressForm />
-        default:
-            return null;
+class AddressSteps extends React.Component<IAddressStepsProps, {}> {
+    handleAddressFormSubmit(values: IAddressFormValues) {
+        console.log(values);
+    }
+
+    render() {
+        switch (this.props.currentStep) {
+            case 1:
+                return <AddressForm onSubmit={this.handleAddressFormSubmit} />
+            default:
+                return null;
+        }
     }
 }
+
+export default AddressSteps;
