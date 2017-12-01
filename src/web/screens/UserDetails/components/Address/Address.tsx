ddb32f165b2fbf8c75505c0bf33c8a9dd62c0e06
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-12-01 10:29:51 +0300

    Add buttons

--- src/web/screens/UserDetails/components/Address/Address.tsx
+++ src/web/screens/UserDetails/components/Address/Address.tsx
@@ -6,11 +6,18 @@ import {connect} from 'react-redux';
 import {IReduxState} from '~/common/interfaces/store';
 import {IAddress} from '~/common/interfaces/userDetails';
 import {getAddress} from '~/common/selectors/userDetils';
+import styled from 'styled-components';
+import RoundedButton from 'src/web/components/RoundedButton';
 
 interface IAddressProps {
     className: string;
     address: IAddress;
 }
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 const Address = (props: IAddressProps) => {
     const {
@@ -29,6 +36,23 @@ const Address = (props: IAddressProps) => {
             <PrepopulatedField value={line_1}/>
             <FieldTitle>Line 2</FieldTitle>
             <PrepopulatedField value={line_2}/>
+            <ButtonsContainer>
+                <RoundedButton
+                    label="Back"
+                    disabled={false}
+                    style={{
+                        marginRight: 20,
+                        width: 200,
+                    }}
+                />
+                <RoundedButton
+                    label="Submit Address"
+                    disabled={false}
+                    style={{
+                        width: 200,
+                    }}
+                />
+            </ButtonsContainer>
         </div>
     )
 }
