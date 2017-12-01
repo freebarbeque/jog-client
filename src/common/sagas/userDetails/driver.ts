7e4bd70b40b1807c23ccd75cbdfcd2501f8c8a89
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-12-01 15:23:53 +0300

    Define watchSelectedDriverChange saga

--- src/common/sagas/userDetails/driver.ts
+++ src/common/sagas/userDetails/driver.ts
@@ -1,10 +1,16 @@
-const {cancel, fork, put, race, select, take} = require('redux-saga/effects');
+const {cancel, fork, put, race, select, take, takeEvery} = require('redux-saga/effects');
 import {LOCATION_CHANGE, push} from 'react-router-redux';
 import {setIsLoading, storeDriverLocally} from '../../actions/userDetails';
-import {SUBMIT_DRIVER} from '../../constants/userDetails';
+import {CHANGE_SELECTED_DRIVER, SUBMIT_DRIVER} from '../../constants/userDetails';
 import {mapDriverDetailsFormValues} from '../../utils/userDetails';
 
+function* watchSelectedDriverChange() {
+    console.log('Will change form values');
+}
+
 function* driverWorker(policyId: string) {
+    yield fork(takeEvery, CHANGE_SELECTED_DRIVER, watchSelectedDriverChange);
+
     while (true) {
         const {driver} = yield take(SUBMIT_DRIVER);
         yield put(storeDriverLocally(policyId, mapDriverDetailsFormValues(driver)));
