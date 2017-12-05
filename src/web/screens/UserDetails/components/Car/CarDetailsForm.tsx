401c4218ee43df16b6af94dc58a2e02bd8e50b6a
Denis Polyak <denispolyak@Deniss-Mac-mini.local>
2017-12-05 19:07:39 +0300

    add non-empty validation to registration number field

--- src/web/screens/UserDetails/components/Car/CarDetailsForm.tsx
+++ src/web/screens/UserDetails/components/Car/CarDetailsForm.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import {reduxForm, Field} from 'redux-form';
-import DatePicker from 'src/web/components/PolicyDatePicker';
 import FieldTitle from '../FieldTitle';
 import styledForm from '../styledForm';
 import StyledInput from '../StyledInput';
@@ -9,6 +8,7 @@ import RoundedButton from 'src/web/components/RoundedButton';
 import {connect} from 'react-redux';
 import {IReduxState} from '~/common/interfaces/store';
 import {getIsLoading} from '~/common/selectors/userDetils';
+const validate = require('validate.js');
 
 interface ICardDetailsForm {
     className?: string;
@@ -51,6 +51,20 @@ class CarDetailsForm extends React.Component<ICardDetailsForm, {}> {
     }
 }
 
+const validationSchema = {
+    vrm: {
+        presence: {
+            message: 'Please enter your registration number',
+            allowEmpty: false,
+        },
+    },
+};
+
+const validateForm = (values: any) => {
+    const errors = validate(values, validationSchema, {fullMessages: false});
+    return errors;
+};
+
 const StyledCarDetailsForm = styledForm(CarDetailsForm as any);
 
 const FieldContainer = styled.div`
@@ -73,6 +87,7 @@ const mapStateToProps = (state: IReduxState) => ({
 
 const form = reduxForm({
     form: 'carDetailsForm',
+    validate: validateForm,
 })(StyledCarDetailsForm);
 
 export default connect(mapStateToProps, null)(form) as any;
