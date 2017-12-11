5f227839dca994943c9f2984f4cd5b6cd729a8b4
romansilence <roman.borodavko@gmail.com>
2017-12-11 11:30:48 +0300

    add link to logo

--- src/web/components/Header.tsx
+++ src/web/components/Header.tsx
@@ -35,7 +35,8 @@ interface IHeaderProps {
 
 const Header = (props: IHeaderProps) => (
     <div className={props.className}>
-        <Logo />
+        
+        <Link to="/"><Logo /></Link>
         <LinksContainer>
             <Link to="/">Policies</Link>
             <Link to="/">Markets</Link>
