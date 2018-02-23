0e50bdfe5bb96b0de984c19214ed2dd5d61cad79
Quique Borredá <qborreda@gmail.com>
2018-02-23 13:49:36 +0100

    Checks wether expiry is a moment object before formatting it.

--- src/common/utils/policies.ts
+++ src/common/utils/policies.ts
@@ -1,11 +1,15 @@
-import {ICreatePolicyFormValues, IPatchPolicyFormValues, IPolicy} from '../interfaces/policies';
-import {format} from 'date-fns';
+import {
+    ICreatePolicyFormValues,
+    IPatchPolicyFormValues,
+    IPolicy,
+} from '../interfaces/policies';
+import { format } from 'date-fns';
 
 export function mapCreatePolicyFormValues(values: ICreatePolicyFormValues) {
-    const {day, month, year, ...rest} = values;
+    const { day, month, year, ...rest } = values;
     const mappedValues: Partial<IPolicy> = rest;
 
-    if (rest.expiry) {
+    if (rest.expiry && rest.expiry.format) {
         mappedValues.expiry = rest.expiry.format('YYYY-MM-DD');
     }
     mappedValues.no_claims_bonus = values.no_claims_bonus || 0;
@@ -15,18 +19,19 @@ export function mapCreatePolicyFormValues(values: ICreatePolicyFormValues) {
 }
 
 export const mapPatchPolicyFormValues = (values: IPatchPolicyFormValues) => {
-    const {day, month, year, ...rest} = values;
+    const { day, month, year, ...rest } = values;
     const mappedValues: Partial<IPolicy> = rest;
 
-    if (rest.expiry) {
+    if (rest.expiry && rest.expiry.format) {
         mappedValues.expiry = rest.expiry.format('YYYY-MM-DD');
     }
     if (rest.annual_cost_cents) {
-      mappedValues.annual_cost_cents = Number(values.annual_cost_cents) * 100;
+        mappedValues.annual_cost_cents = Number(values.annual_cost_cents) * 100;
     }
     if (rest.excess_amount_cents) {
-      mappedValues.excess_amount_cents = Number(values.excess_amount_cents) * 100;
+        mappedValues.excess_amount_cents =
+            Number(values.excess_amount_cents) * 100;
     }
 
     return mappedValues;
-}
+};
